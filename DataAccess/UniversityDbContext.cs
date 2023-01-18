9abb7f3b5dc6887776080a59ffde6404c802119f
Alfredo Aguirre <alfaguir@pichincha.com>
2023-01-18 17:26:39 -0500

    Agregar archivos de proyecto.

---
+++ DataAccess/UniversityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UniversityWebAPI.DataAccess
+{
+    public class UniversityDbContext :DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options): base(options)
+        {
+
+        }
+
+        // TODO: Add DbSets (Tables of our Data base)
+
+    }
+}
