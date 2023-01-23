268d788a707884fa331c996c10123746ef606ada
Alfredo Aguirre <thefurok@gmail.com>
2023-01-23 01:55:43 -0500

    Create a student model

---
+++ Models/DataModels/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityWebAPI.Models.DataModels
+{
+    public class Student : BaseEntity
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public DateTime DateOfBirtg { get; set; }
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+
+    }
+}
